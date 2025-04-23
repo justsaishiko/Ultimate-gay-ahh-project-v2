8ec2bceb803fd777d5fd488ab07fbebd6677fe95
Saishiko <ka.slusarz@wp.pl>
2025-04-23 09:53:47 +0200

    skibi i have gone insane why am i going tis

---
+++ server.js
@@ -0,0 +1,28 @@
+const hbs = require('express-handlebars');
+const express = require("express")
+const app = express()
+const PORT = 3000;
+const path = require("path")
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('hbs', hbs({ 
+    defaultLayout: 'base.hbs',
+    extname: '.hbs',
+    partialsDir: "views/partials",
+
+}));
+
+Handlebars.registerPartial('sideBar', '{{sidebae}}');
+Handlebars.registerPartial('navbar', '{{nave}}');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function (req, res) {
+    res.render('main.hbs');
+})
+
+app.listen(PORT, function () {
+    console.log("start serwera na porcie " + PORT )
+})
+
