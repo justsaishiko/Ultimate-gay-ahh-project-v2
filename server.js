16a27cdfd0448e181e6d32984b3cb1ca09c0f647
Saishiko <ka.slusarz@wp.pl>
2025-04-30 10:49:39 +0200

    got freaky w it, added page icns, easter egg :P, no liks yet gna do that in a min

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const PORT = 3000;
 const path = require("path");
 const { handlebars } = require('hbs');
 
+app.use(express.static(__dirname + '/public'));
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.engine('hbs', hbs({ 
     defaultLayout: 'base.hbs',
@@ -16,11 +19,14 @@ app.engine('hbs', hbs({
 }));
 
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
 app.get("/", function (req, res) {
     res.render('main.hbs');
 })
+
+
+
+
 
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT )
